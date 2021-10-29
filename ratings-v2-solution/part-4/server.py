0011a3839a87440b42448defd3ce84e952a3a6ea
Vallie Tracy <trac0055@umn.edu>
2021-10-29 15:53:04 -0500

    finished the lab; solution folder was added

--- ratings-v2-solution/part-4/server.py
+++ ratings-v2-solution/part-4/server.py
@@ -1,20 +1,49 @@
 """Server for movie ratings app."""
 
-from flask import (Flask, render_template, request, flash, session,
-                   redirect)
+from flask import Flask, render_template, request, flash, session, redirect
 from model import connect_to_db
 import crud
+
 from jinja2 import StrictUndefined
 
 app = Flask(__name__)
 app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
 
-@app.route('/')
+
+@app.route("/")
 def homepage():
     """View homepage."""
 
-    return render_template('homepage.html')
+    return render_template("homepage.html")
+
+
+@app.route("/movies")
+def all_movies():
+    """View all movies."""
+
+    movies = crud.get_movies()
+
+    return render_template("all_movies.html", movies=movies)
+
+
+@app.route("/movies/<movie_id>")
+def show_movie(movie_id):
+    """Show details on a particular movie."""
+
+    movie = crud.get_movie_by_id(movie_id)
+
+    return render_template("movie_details.html", movie=movie)
+
+
+@app.route("/users")
+def all_users():
+    """View all users."""
+
+    users = crud.get_users()
+
+    return render_template("all_users.html", users=users)
+
 
 @app.route("/users", methods=["POST"])
 def register_user():
@@ -31,6 +60,16 @@ def register_user():
         flash("Account created! Please log in.")
 
     return redirect("/")
+
+
+@app.route("/users/<user_id>")
+def show_user(user_id):
+    """Show details on a particular user."""
+
+    user = crud.get_user_by_id(user_id)
+
+    return render_template("user_details.html", user=user)
+
 
 @app.route("/login", methods=["POST"])
 def process_login():
@@ -49,35 +88,6 @@ def process_login():
 
     return redirect("/")
 
-@app.route("/movies")
-def all_movies():
-    """View all movies."""
-
-    movies = crud.get_movies()
-
-    return render_template("all_movies.html", movies=movies)
-
-@app.route("/movies/<movie_id>")
-def show_movie(movie_id):
-    """Show details on a particular movie."""
-
-    movie = crud.get_movie_by_id(movie_id)
-
-    return render_template("movie_details.html", movie=movie)
-
-@app.route("/users")
-def all_users():
-
-    users= crud.get_users()
-
-    return render_template("all_users.html", users=users)
-
-@app.route("/users/<user_id>")
-def show_user_profile(user_id):
-
-    user = crud.get_user_by_id(user_id)
-
-    return render_template("user_details.html", user=user)
 
 @app.route("/movies/<movie_id>/ratings", methods=["POST"])
 def create_rating(movie_id):
@@ -102,7 +112,5 @@ def create_rating(movie_id):
 
 
 if __name__ == "__main__":
-    # DebugToolbarExtension(app)
     connect_to_db(app)
     app.run(host="0.0.0.0", debug=True)
-
